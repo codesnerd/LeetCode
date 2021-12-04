32b936819f929938e14996cb6f89c5b26752d54e
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-12-04 17:16:22 +0500

    Add remove duplicates

---
+++ Top Interview Questions - Easy Collection/Array/1- Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+
+// O(n)
+int removeDuplicates(vector<int>& nums)
+{
+    int j = 0;
+    if (nums.size())
+    {
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != nums[j])
+                nums[++j] = nums[i];
+        }
+
+        j++;
+    }
+
+    return j;
+}
