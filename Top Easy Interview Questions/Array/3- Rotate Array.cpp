8e64d3829ed4d51da1a2c9009bb76e68c093f440
codesnerd <70039999+codesnerd@users.noreply.github.com>
2022-05-06 00:01:38 +0500

    Add rotate array optimized solution

--- Top Easy Interview Questions/Array/3- Rotate Array.cpp
+++ Top Easy Interview Questions/Array/3- Rotate Array.cpp
@@ -1,9 +1,10 @@
 // Problem: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
 
 /*
-* Time Complexity: O(n)
-* Space Complexity: O(n)
-*/
+ * --- SOLUTION 1 ---
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
 void rotate(vector<int> &nums, int k)
 {
     vector<int> newArray(nums.size());
@@ -14,4 +15,30 @@ void rotate(vector<int> &nums, int k)
     }
 
     nums = newArray;
+}
+
+/*
+ * --- SOLUTION 2 ---
+ * Time Complexity: O(n)
+ * Space Complexity: O(1) In-place
+ */
+
+/* HELPER FUNCTION */
+void reverse(vector<int> &arr, int start, int end)
+{
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void rotate(vector<int> &nums, int k)
+{
+    int size = nums.size();
+    k %= size;
+    reverse(nums, 0, size - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, size - 1);
 }
