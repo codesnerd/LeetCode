4f8a878e298715d7f9f8cc16913ea21d5b5c3088
codesnerd <70039999+codesnerd@users.noreply.github.com>
2022-05-10 00:46:13 +0500

    Update contains duplicate time complexity

--- Top Easy Interview Questions/Array/4- Contains Duplicate.cpp
+++ Top Easy Interview Questions/Array/4- Contains Duplicate.cpp
@@ -1,7 +1,7 @@
 // Problem: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
 
 /*
- * Time Complexity: O(nlogn) - Unordered vector to set (https://stackoverflow.com/a/63121302/11917891)
+ * Time Complexity: O(n) - Unordered vector to unordered set (https://stackoverflow.com/a/63121302/11917891)
  * Space Complexity: O(n)
  */
 bool containsDuplicate(vector<int> &nums)
